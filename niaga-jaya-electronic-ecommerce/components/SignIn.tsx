ec9cb1ff9e38951d4c3ca5f97ff2feffde488971
Fadil <fadilvtr23@gmail.com>
2026-04-29 16:45:33 +0700

    fix signin

--- niaga-jaya-electronic-ecommerce/components/SignIn.tsx
+++ niaga-jaya-electronic-ecommerce/components/SignIn.tsx
@@ -1,30 +1,28 @@
-// components/SignIn.tsx
 "use client";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
 
 const SignIn = () => {
+  const { isSignedIn } = useUser();
+
   return (
     <div className="flex items-center">
-      {/* Tampil jika user belum login */}
-      <SignedOut>
+      {!isSignedIn ? (
         <SignInButton mode="modal">
           <button className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg text-sm font-medium transition-all">
             Login
           </button>
         </SignInButton>
-      </SignedOut>
-
-      {/* Tampil jika user sudah login (Foto Profil) */}
-      <SignedIn>
-        <UserButton 
-          afterSignOutUrl="/" 
+      ) : (
+        <UserButton
+          afterSignOutUrl="/"
           appearance={{
             elements: {
-              avatarBox: "h-9 w-9" // Biar ukurannya pas dengan icon lainnya
-            }
+              avatarBox: "h-9 w-9",
+            },
           }}
         />
-      </SignedIn>
+      )}
     </div>
   );
 };
